09838ac362d99865dbebde6d67916b246f644baf
Lucile <lucile.1988.ls@gmail.com>
2016-05-07 20:26:29 +0200

    Dynamic categories and items (model).

--- public/cart/cart.js
+++ public/cart/cart.js
@@ -9,6 +9,39 @@ angular.module('cart', ['ngRoute'])
   });
 }])
 
-.controller('CartCtrl', [function() {
-    
+.controller('CartCtrl', ['$scope', function($scope) {
+    $scope.categories = [
+      {
+        name: 'Cuisine',
+        items: [
+          { name: 'Four', amount: 600 },
+          { name: 'Frigo', amount: 200 },
+          { name: 'Micro-ondes', amount: 75 }
+        ]
+      },
+      {
+        name: 'Salon',
+        items: [
+          { name: 'Canape', amount: 400},
+          { name: 'Table basse', amount: 150 },
+          { name: 'TV', amount: 380 }
+        ]
+      },
+      {
+        name: 'Salle de bains',
+        items: [
+          { name: 'Lavabo', amount: 150 },
+          { name: 'Douche Italienne', amount: 1200 },
+          { name: 'WC', amount: 275 }
+        ]
+      },
+      {
+        name: 'Chambre',
+        items: [
+          { name: 'Lit', amount: 800},
+          { name: 'Armoire', amount: 350 },
+          { name: 'Table de chevet', amount: 65 }
+        ]
+      }
+    ];
 }]);
